71002ac0a71311b0bf3526c673ec3ee3d48d5a10
Igor Siveroni <igor@siveroni.com>
2021-04-19 15:53:47 +0100

    v1.3.7 patch 2

--- src/phydyn/datafit/SeroprevalenceLikelihood.java
+++ src/phydyn/datafit/SeroprevalenceLikelihood.java
@@ -10,45 +10,76 @@ import phydyn.util.DVector;
 import phydyn.util.General;
 
 public class SeroprevalenceLikelihood extends TrajectoryFit {
-	public Input<Double> sigmaInput = new Input<>("sigma", "Standard deviation used for normal distribution",Validate.REQUIRED);
+	public Input<Double> sigmaInput = new Input<>("sigma", "Standard deviation used for normal distribution");
 	public Input<Double> popSizeInput = new Input<>("popSize", "Population size used to compute seroprevalence",Validate.REQUIRED);
+	
+	
 	public Input<String> yinfectionsInput = new Input<>("yinfections","Name of ODE variable denoting the total infected population",new String("infections"));
-
+	
+	public Input<String> timeHeaderInput = new Input<>("timeHeader","Name of time data column header",new String("time"));
+	public Input<String> prevalenceHeaderInput = new Input<>("prevalenceHeader","Name of seroprevalence data column header",new String("sp"));
+	public Input<String> sigmaHeaderInput = new Input<>("sigmaHeader","Name of seroprevalence data header",new String("sigma"));
+	
 	
 	public Input<Boolean> quietInput = new Input<>("quiet","Print seropevalences",new Boolean(true));
 	public Input<Boolean> ignoreInput = new Input<>("ignore","Ignore SeroP likelihood",new Boolean(false));
 	
 	double popSize;
-	double sigma;
+	
 
 	boolean quiet, ignore;
-	double[] colTime;
-	double[] colSP;
+	double[] colTime, colSP, colSigmas;
+	//double[] colSP;
+	//double[] colSigmas;
 	
 	//org.apache.commons.math.distribution.NormalDistribution dist;	
 	NormalDistribution[] dist;
+	
 	String[] yNames;
 	int[] idxs;
 	int idxIl, idxIh, idxInfections;
+	int idxTime, idxPrevalence, idxSigma;
 
 	@Override
 	public void initAndValidate() {
 		super.initAndValidate();
-		sigma = sigmaInput.get();
+		
 		popSize = popSizeInput.get();
 		
 		quiet = quietInput.get();
 		ignore = ignoreInput.get();
 		
-		colTime = this.getColumnAsDouble(0);
-		colSP = this.getColumnAsDouble(1);
-		
+		idxTime = General.indexOf(timeHeaderInput.get(), headers);
+		if (idxTime == -1)
+			throw new IllegalArgumentException("SeroPrevalenceLikelihood: Couldn't find time data column: "+timeHeaderInput.get());
+		idxPrevalence = General.indexOf(prevalenceHeaderInput.get(), headers);
+		if (idxPrevalence == -1)
+			throw new IllegalArgumentException("SeroPrevalenceLikelihood: Couldn't find sero-prevalence data column: "+prevalenceHeaderInput.get());		
+		colTime = this.getColumnAsDouble(idxTime);
+		colSP = this.getColumnAsDouble(idxPrevalence);		
 		
 		/*
 		 * Distribution parameters
 		 * Currently sigma. They could be passed in a parameter vector.
 		 */
 		//System.out.println("sigma="+sigma+"  popSize="+popSize);
+		
+		colSigmas = null;
+		idxSigma = General.indexOf("sigma", headers);
+		if (sigmaInput.get() == null) {
+			if (idxSigma == -1)
+				throw new IllegalArgumentException("SeroprevalenceLikelihood: No 'sigma' parameter or data column provided");
+			colSigmas = this.getColumnAsDouble(idxSigma);			
+		} else {
+			if (idxSigma != -1) {
+				System.out.println("-- WARNING (SeroprevalenceLikelihood): Both 'sigma' parameter or data column provided");
+				System.out.println("---- Using 'sigma' parameter value for all distributions");
+			}
+			final double sgm = sigmaInput.get();
+			colSigmas = new double[numrows];
+			for(int i=0; i < numrows; i++)
+				colSigmas[i] = sgm;
+		}
 		
 		/*
 		 * one distribution per datapoint
@@ -60,7 +91,7 @@ public class SeroprevalenceLikelihood extends TrajectoryFit {
 			//System.out.println(colTime[i]+" - "+colSP[i]);
 			final double dataSP = colSP[i];
 			// one distribution per datapoint 
-			dist[i] = new NormalDistributionImpl(dataSP, sigma);
+			dist[i] = new NormalDistributionImpl(dataSP, colSigmas[i]);
 		}
 				
 		/*
@@ -147,7 +178,8 @@ public class SeroprevalenceLikelihood extends TrajectoryFit {
 			sp = (infections/popSize)*100;
 			//dist = new NormalDistributionImpl(dataSP, sigma);
 			//logP += Math.log(dist[i].density(sp));
-			logP += -Math.log(sigma*Math.sqrt(2*Math.PI))- 0.5*(sp - dataSP)*(sp-dataSP)/(sigma*sigma);
+			final double sgm = colSigmas[i];
+			logP += -Math.log(sgm*Math.sqrt(2*Math.PI))- 0.5*(sp - dataSP)*(sp-dataSP)/(sgm*sgm);
 		}
 		if (!quiet)
 			System.out.println("serop="+ sp +"  seroP logP = "+logP);	
